58c0d6247ed39199b6c62863a3af95dbf2533cca
JOHN RAFAEL ALEJANDRINO <167075750+jeeracd@users.noreply.github.com>
2026-03-27 09:25:44 +0800

    Updated tokenizer

--- main.go
+++ main.go
@@ -71,3 +71,25 @@ func tokenize(input string) []token {
 
 	return tokens // Return the list of tokens
 }
+
+func isNumber(char string) bool {
+	if char == "" {
+		return false
+	}
+	n := []rune(char)[0]
+	if n >= '0' && n <= '9' {
+		return true
+	}
+	return false
+}
+
+func isLetter(char string) bool {
+	if char == "" {
+		return false
+	}
+	n := []rune(char)[0]
+	if n >= 'a' && n <= 'z' || n >= 'A' && n <= 'Z' {
+		return true
+	}
+	return false
+}
