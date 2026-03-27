371624673e07fe3a9c400fd08d503035532560a1
JOHN RAFAEL ALEJANDRINO <167075750+jeeracd@users.noreply.github.com>
2026-03-27 09:19:25 +0800

    Added Tokenizer

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+type token struct {
+	kind  string
+	value string
+}
+
+func tokenize(input string) []token {
+
+	input += "\n"
+
+	now := 0
+
+	tokens := []token{}
+	for now < len([]rune(input)) {
+		char := string(input[now])
+
+		if char == "(" { // Checks for left parenthesis
+			tokens = append(tokens, token{kind: "lparen", value: char})
+
+			now++
+			continue
+		}
+
+		if char == ")" { // Checks for right parenthesis
+			tokens = append(tokens, token{kind: "rparen", value: char})
+
+			now++
+			continue
+		}
+
+		if char == "\n" || char == " " || char == "\t" { // Checks for whitespace, then ignore
+			now++
+			continue
+		}
+
+		if isNumber(char) { // Checks for numbers, then adds them to the token list
+			num := ""
+
+			for isNumber(char) {
+				value += char
+				now++
+				char = string([]rune(input)[now])
+			}
+
+			tokens = append(tokens, token{
+				kind:  "number",
+				value: value,
+			})
+
+			continue
+		}
+
+		if isLetter(char) { // Checks for symbols, then adds them to the token list
+			value := ""
+
+			for isLetter(char) {
+				value += char
+				now++
+				char = string([]rune(input)[now])
+			}
+
+			tokens = append(tokens, token{
+				kind:  "symbol",
+				value: value,
+			})
+			continue
+		}
+		break
+	}
+
+	return tokens // Return the list of tokens
+}
